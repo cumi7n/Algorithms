f1dbb6c6cf99479d1027f7fca9427ef514f8cd8a
Andrey <104021106+cumi7n@users.noreply.github.com>
2024-11-03 10:11:55 +0300

    Update class1_least absolute difference.cpp

--- class1_least absolute difference.cpp
+++ class1_least absolute difference.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(){
-	int n = 5;
+	int n = 5; 
 	int m = 4; 
 	int a[5] = {1, 3, 5, 7, 9}; 
 	int b[4] = {2, 4, 6, 8}; 
@@ -12,7 +12,13 @@ int main(){
 	int i = 0 , j = 0, mn = INT_MAX;
 	
 	while (i < n && j < m){
-		mn = abs(a[i] - b[j]);
+	
+		int min_mn = mn = abs(a[i] - b[j]);
+
+		if (min_mn < mn){
+			mn = min_mn;
+		}
+
 		if ( i < n && a[i] < b[j]){
 			i++ ;
 		}else{
@@ -21,4 +27,3 @@ int main(){
 	}
 	cout << mn;
 }
-
